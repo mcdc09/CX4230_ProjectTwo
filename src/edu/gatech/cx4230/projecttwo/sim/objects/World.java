a423af901e0fff69cd357085814097c616fc014e
allichis <gth708g@gatech.edu>
2014-03-31 23:22:11 -0400

    update World - added getters/setters

--- src/edu/gatech/cx4230/projecttwo/sim/objects/World.java
+++ src/edu/gatech/cx4230/projecttwo/sim/objects/World.java
@@ -1,5 +1,35 @@
 package edu.gatech.cx4230.projecttwo.sim.objects;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class World {
 
+	private ArrayList<Airport> airports;
+	private int currentSimTime;
+	
+	public World() {
+		airports = new ArrayList<Airport>();
+		currentSimTime = 0;
+	}
+	
+	public void addAirport(Airport airport) {
+		airports.add(airport);
+	}
+	
+	public ArrayList<Airport> getAirports() {
+		return airports;
+	}
+	
+	public void setAirports(ArrayList<Airport> airports) {
+		this.airports = airports;
+	}
+	
+	public int getCurrentSimTime() {
+		return currentSimTime;
+	}
+	
+	public void setCurrentSimTime(int time) {
+		currentSimTime = time;
+	}
 }
