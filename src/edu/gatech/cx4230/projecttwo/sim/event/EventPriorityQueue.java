87632b23f80b6ee323c1bfb106ad6570cc3f0e75
allichis <gth708g@gatech.edu>
2014-03-31 23:35:35 -0400

    update EventPriorityQueue - implemented addInOrder, removeMin, and getMinValue

--- src/edu/gatech/cx4230/projecttwo/sim/event/EventPriorityQueue.java
+++ src/edu/gatech/cx4230/projecttwo/sim/event/EventPriorityQueue.java
@@ -2,23 +2,30 @@ package edu.gatech.cx4230.projecttwo.sim.event;
 
 import java.util.ArrayList;
 
-public class EventPriorityQueue<Event> {
+public class EventPriorityQueue<T> {
 	
 	ArrayList<Event> events;
 	
 	public EventPriorityQueue() {
-		
+		events = new ArrayList<Event>();
 	}
 	
 	public void addInOrder(Event e) {
-		
+		int insertIndex = events.size();
+		for(int i = 0; i < events.size(); i++) {
+			if(e.getProcessTime() < events.get(i).getProcessTime()) {
+				insertIndex = i;
+				break;
+			}
+		}
+		events.add(insertIndex, e);
 	}
 	
 	public Event removeMin() {
-		return null;
+		return events.remove(0);
 	}
 
 	public int getMinValue() {
-		return 0;
+		return events.get(0).getProcessTime();
 	}
 }
