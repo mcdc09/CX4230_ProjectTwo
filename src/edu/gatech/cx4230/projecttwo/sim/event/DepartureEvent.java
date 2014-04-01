eb1b6fde40da80df2112cf389da862a7c8eac79a
allichis <gth708g@gatech.edu>
2014-03-31 22:07:26 -0400

    update Events - added constructors

--- src/edu/gatech/cx4230/projecttwo/sim/event/DepartureEvent.java
+++ src/edu/gatech/cx4230/projecttwo/sim/event/DepartureEvent.java
@@ -1,7 +1,15 @@
 package edu.gatech.cx4230.projecttwo.sim.event;
 
+import edu.gatech.cx4230.projecttwo.sim.objects.Flight;
+
 public class DepartureEvent extends Event {
 
+	public DepartureEvent(Flight flight, int creationTime, int processTime) {
+		this.flight = flight;
+		this.creationTime = creationTime;
+		this.processTime = processTime;
+	}
+	
 	@Override
 	public void process() {
 		// TODO Auto-generated method stub
--- src/edu/gatech/cx4230/projecttwo/sim/event/LandedEvent.java
+++ src/edu/gatech/cx4230/projecttwo/sim/event/LandedEvent.java
@@ -1,7 +1,15 @@
 package edu.gatech.cx4230.projecttwo.sim.event;
 
+import edu.gatech.cx4230.projecttwo.sim.objects.Flight;
+
 public class LandedEvent extends Event {
 
+	public LandedEvent(Flight flight, int creationTime, int processTime) {
+		this.flight = flight;
+		this.creationTime = creationTime;
+		this.processTime = processTime;
+	}
+	
 	@Override
 	public void process() {
 		// TODO Auto-generated method stub
