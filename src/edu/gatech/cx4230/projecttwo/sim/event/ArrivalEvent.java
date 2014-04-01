879e6ec324232bcb9be34970dfe71b153892f869
allichis <gth708g@gatech.edu>
2014-03-31 22:06:12 -0400

    update ArrivalEvent - process() method

--- src/edu/gatech/cx4230/projecttwo/sim/event/ArrivalEvent.java
+++ src/edu/gatech/cx4230/projecttwo/sim/event/ArrivalEvent.java
@@ -1,11 +1,34 @@
 package edu.gatech.cx4230.projecttwo.sim.event;
 
+import edu.gatech.cx4230.projecttwo.sim.objects.Airport;
+import edu.gatech.cx4230.projecttwo.sim.objects.Flight;
+import edu.gatech.cx4230.projecttwo.sim.objects.Runway;
+
 public class ArrivalEvent extends Event {
 
+	public ArrivalEvent(Flight flight, int creationTime, int processTime) {
+		this.flight = flight;
+		this.creationTime = creationTime;
+		this.processTime = processTime;
+	}
+	
 	@Override
 	public void process() {
 		// TODO Auto-generated method stub
+		Airport destination = flight.getDestination();
+		int inAir = destination.getInTheAir();
+		destination.setInTheAir(++inAir);
 		
+		Runway runway = destination.getFreeRunway();
+		if(runway != null) {
+			runway.setRunwayFree(false);
+			runway.setAircraft(flight.getAircraft());
+			
+			int currSimTime = 0; // TODO get current sim time
+			int processTime = currSimTime + 1; // TODO when should the landing event be processed?
+			LandedEvent landEvent = new LandedEvent(flight, currSimTime, processTime);
+			destination.addPendingEvent(landEvent);
+		}
 	}
 
 }
