7e93f543bde34857e74c3d863c83f5947b3aa29e
Joseph Mattingly <jmattingly13@gmail.com>
2014-04-04 10:57:04 -0400

    New timetable object

--- src/edu/gatech/cx4230/projecttwo/sim/creation/FlightGenerator.java
+++ src/edu/gatech/cx4230/projecttwo/sim/creation/FlightGenerator.java
@@ -1,5 +1,8 @@
 package edu.gatech.cx4230.projecttwo.sim.creation;
 
 public class FlightGenerator {
-
+	
+	public FlightGenerator(){
+		
+	}
 }
---
+++ src/edu/gatech/cx4230/projecttwo/sim/event/Timetable.java
@@ -0,0 +1,13 @@
+package edu.gatech.cx4230.projecttwo.sim.event;
+
+public class Timetable {
+	private int departureTime;
+	
+	public Timetable() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getDepartureTime(){
+		return departureTime;
+	}
+}
