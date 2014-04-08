d80af0687f4f14be3a544863b49eb77b12687b30
mcdc09 <ThomasBowling@Comcast.net>
2014-04-08 10:43:52 -0400

    Vis: Continued Work

--- src/edu/gatech/cx4230/projecttwo/vis/map/SimMap.java
+++ src/edu/gatech/cx4230/projecttwo/vis/map/SimMap.java
@@ -1,14 +1,37 @@
 package edu.gatech.cx4230.projecttwo.vis.map;
 
+import java.util.List;
+
 import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.marker.Marker;
+import de.fhpotsdam.unfolding.marker.MarkerManager;
 import de.fhpotsdam.unfolding.utils.MapUtils;
+import edu.gatech.cx4230.projecttwo.sim.objects.Airport;
+import edu.gatech.cx4230.projecttwo.sim.objects.Flight;
+import edu.gatech.cx4230.projecttwo.vis.creation.AirportMarkerCreator;
+import edu.gatech.cx4230.projecttwo.vis.creation.FlightMarkerCreator;
+import edu.gatech.cx4230.projecttwo.vis.markers.AirportMarker;
+import edu.gatech.cx4230.projecttwo.vis.markers.FlightRouteMarker;
 
 public class SimMap {
 	private UnfoldingMap map;
+	private MarkerManager<Marker> manager;
 	public static final int MAP_X = 25, MAP_Y = 25, MAP_WIDTH = 600, MAP_HEIGHT = 500;
 	
 	public SimMap(VisPApplet vis) {
 		map = new UnfoldingMap(vis, MAP_X, MAP_Y, MAP_WIDTH, MAP_HEIGHT);
+		manager = map.getDefaultMarkerManager();
+		
+		List<Airport> airports = null; // TODO
+		List<Flight> flights = null; // TODO
+		
+		AirportMarkerCreator amc = new AirportMarkerCreator(airports);
+		List<AirportMarker> airportMarkers = amc.getAirportMarkers();
+		
+		
+		FlightMarkerCreator fmc = new FlightMarkerCreator(flights);
+		List<FlightRouteMarker> flightRouteMarkers  = fmc.getFlightMarkers();
+		
 		
 		MapUtils.createDefaultEventDispatcher(vis, map);
 	}
--- src/edu/gatech/cx4230/projecttwo/vis/markers/AirportMarker.java
+++ src/edu/gatech/cx4230/projecttwo/vis/markers/AirportMarker.java
@@ -1,16 +1,16 @@
 package edu.gatech.cx4230.projecttwo.vis.markers;
 
-import processing.core.PGraphics;
 import de.fhpotsdam.unfolding.geo.Location;
-import de.fhpotsdam.unfolding.marker.AbstractMarker;
+import de.fhpotsdam.unfolding.marker.SimplePointMarker;
 
-public class AirportMarker extends AbstractMarker {
+public class AirportMarker extends SimplePointMarker {
 	private final String icaoCode;
+	private static final float RAD = 5;
 	
 	public AirportMarker(Location location, String icaoCode) {
 		super(location);
 		this.icaoCode = icaoCode;
-		
+		setRadius(RAD);
 	}
 
 	/**
@@ -18,18 +18,6 @@ public class AirportMarker extends AbstractMarker {
 	 */
 	public String getIcaoCode() {
 		return icaoCode;
-	}
-
-	@Override
-	public void draw(PGraphics arg0, float arg1, float arg2) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	protected boolean isInside(float arg0, float arg1, float arg2, float arg3) {
-		// TODO Auto-generated method stub
-		return false;
 	}
 
 }
