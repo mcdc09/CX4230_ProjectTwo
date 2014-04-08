dbcb406fedee9885cc8c87b264328dfcf1919602
Joseph Mattingly <jmattingly13@gmail.com>
2014-04-08 11:56:21 -0400

    flight scheduling basics
    still can't schedule a flight, but getting there.

--- src/edu/gatech/cx4230/projecttwo/sim/creation/FlightGenerator.java
+++ src/edu/gatech/cx4230/projecttwo/sim/creation/FlightGenerator.java
@@ -2,6 +2,7 @@ package edu.gatech.cx4230.projecttwo.sim.creation;
 
 import java.util.ArrayList;
 
+import sun.security.util.Length;
 import edu.gatech.cx4230.projecttwo.sim.event.Timetable;
 import edu.gatech.cx4230.projecttwo.sim.objects.World;
 import edu.gatech.cx4230.projecttwo.utilities.CSVRow;
@@ -15,13 +16,29 @@ public class FlightGenerator {
 		
 		flightList = (new CSVReaderInitialMatrix()).getRows();
 		
-		for(int i = 0; i < 0; i++){
-			World.chance().nextInt();
-			// t.addScheduledFlight(origin, destination, aircraftType, departureTime);
+		int[][] dm = distanceMatrix();
+		int[][] fm = flightMatrix();
+		int[][] tzm = timezoneMatrix();
+		
+		for(int i = 0; i < fm.length; i++){
+			for(int j = 0; j < fm[i].length; j++){
+				int lengthOfDay = 14 - tzm[i][j];
+				int flightInterval = lengthOfDay * 3600 / fm[i][j];
+				// t.addScheduledFlight(origin, destination, aircraftType, departureTime);
+			}
 		}
 	}
 	
 	private int[][] distanceMatrix(){
+		return null;
+	}
+	
+	private int[][] flightMatrix(){
+		return null;
+	}
+	
+	private int[][] timezoneMatrix(){
+		// Timezone of origin - Timezone of destination
 		return null;
 	}
 }
