e24781f5bdbf71a58e2c8a19d9c3cfbd44d7509f
Joseph Mattingly <jmattingly13@gmail.com>
2014-04-08 09:14:05 -0400

    Added capability to add scheduled flights to the timetable
    Incorporates new object to stick flight info in.  New object has get
    methods for origin, destination, aircraft type, and scheduled departure
    time.

---
+++ src/edu/gatech/cx4230/projecttwo/sim/objects/ScheduledFlight.java
@@ -0,0 +1,31 @@
+package edu.gatech.cx4230.projecttwo.sim.objects;
+
+public class ScheduledFlight {
+	private String origin;
+	private String destination;
+	private String aircraftType;
+	private int departureTime;
+	
+	public ScheduledFlight(String origin, String destination, String aircraftType, int departureTime) {
+		this.origin = origin;
+		this.destination = destination;
+		this.aircraftType = aircraftType;
+		this.departureTime = departureTime;
+	}
+	
+	public String getOrigin(){
+		return origin;
+	}
+	
+	public String getDestination(){
+		return destination;
+	}
+	
+	public String getAircraftType(){
+		return aircraftType;
+	}
+	
+	public int getDepartureTime(){
+		return departureTime;
+	}
+}
