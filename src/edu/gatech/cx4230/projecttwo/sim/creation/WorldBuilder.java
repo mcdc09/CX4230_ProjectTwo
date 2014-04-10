6f5e65e074a737467b412f9c4c14cf2543320103
mcdc09 <ThomasBowling@Comcast.net>
2014-04-09 20:39:24 -0400

    Added Airports to WorldBuilder refs #3

--- src/edu/gatech/cx4230/projecttwo/sim/creation/WorldBuilder.java
+++ src/edu/gatech/cx4230/projecttwo/sim/creation/WorldBuilder.java
@@ -1,5 +1,70 @@
 package edu.gatech.cx4230.projecttwo.sim.creation;
 
-public class WorldBuilder {
+import java.util.ArrayList;
+import java.util.List;
 
+import edu.gatech.cx4230.projecttwo.database.operation.AirportCodeHelper;
+import edu.gatech.cx4230.projecttwo.database.operation.AirportRunwayDataFromDB;
+import edu.gatech.cx4230.projecttwo.database.operation.AirportsDBOperator;
+import edu.gatech.cx4230.projecttwo.sim.objects.Airport;
+import edu.gatech.cx4230.projecttwo.sim.objects.Runway;
+import edu.gatech.cx4230.projecttwo.sim.objects.World;
+
+/**
+ * Creates the world.  Includes Airports with needed info and Runways
+ * @author tbowling3
+ *
+ */
+public class WorldBuilder {
+	private World world;
+
+	public WorldBuilder() {
+		world = new World();
+
+		String[] airportsIncluded = findIncludedAirports();
+
+		AirportsDBOperator airportsDB = new AirportsDBOperator();
+
+		for(String a: airportsIncluded) {
+			ArrayList<Runway> runways = new ArrayList<Runway>();
+			List<AirportRunwayDataFromDB> airportData = airportsDB.getAirportsData(AirportCodeHelper.getICAOcode(a));
+
+			if(airportData.size() > 0) {
+				// Builds List of runways
+				for(AirportRunwayDataFromDB d: airportData) {
+					Runway r = new Runway(d.getRunwayLength());
+					runways.add(r);
+				} // close for
+
+
+				AirportRunwayDataFromDB d = airportData.get(0);
+
+				int maxAircraftCap = 10; // TODO
+				float lat = (float) d.getLatitude();
+				float lon = (float) d.getLongitude();
+				String city = d.getMunicipality();
+				String state = ""; // TODO
+				String icaoCode = d.getAirportID();
+				String timeZone = ""; // TODO
+				
+				Airport airport = new Airport(runways, maxAircraftCap, lat, lon, city, state, icaoCode, timeZone);
+				world.addAirport(airport);
+			} // close if
+		} // close for
+	} // close constructor
+
+
+	private String[] findIncludedAirports() {
+		CSVReaderInitialMatrix reader = new CSVReaderInitialMatrix();
+		List<CSVRowInitialMatrix> rows = reader.getRows();
+		return rows.get(0).getAirports();
+	}
+
+
+	/**
+	 * @return the world
+	 */
+	public World getWorld() {
+		return world;
+	}
 }
