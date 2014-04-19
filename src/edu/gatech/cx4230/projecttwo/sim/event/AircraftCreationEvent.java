b746e97a77b13ecb46463f0ac10f111a4a943d8d
mcdc09 <ThomasBowling@Comcast.net>
2014-04-19 18:12:14 -0400

    Created a AircraftCreationEvent object
    This object is to be used during the initial construction of the
    simulation

---
+++ src/edu/gatech/cx4230/projecttwo/sim/event/AircraftCreationEvent.java
@@ -0,0 +1,60 @@
+package edu.gatech.cx4230.projecttwo.sim.event;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.gatech.cx4230.projecttwo.sim.objects.Aircraft;
+import edu.gatech.cx4230.projecttwo.sim.objects.Airport;
+import edu.gatech.cx4230.projecttwo.sim.objects.LargeAircraft;
+import edu.gatech.cx4230.projecttwo.sim.objects.MediumAircraft;
+import edu.gatech.cx4230.projecttwo.sim.objects.RegionalAircraft;
+import edu.gatech.cx4230.projecttwo.sim.objects.SmallAircraft;
+
+/**
+ * Object used to pass along initial distribution of aircraft to each airport
+ * @author tbowling3
+ *
+ */
+public class AircraftCreationEvent extends AirportEvent {
+	private Airport airport;
+	private int regionalCount;
+	private int smallCount;
+	private int mediumCount;
+	private int largeCount;
+
+	/**
+	 * 
+	 * @param a The airport in question
+	 * @param regionalCount The number of regional aircraft at that airport
+	 * @param smallCount The number of small aircraft aircraft at the airport
+	 * @param medCount The number of medium aircraft aircraft at the airport
+	 * @param largeCount The number of large aircraft aircraft at the airport
+	 */
+	public AircraftCreationEvent(Airport a, int regionalCount, int smallCount, int medCount, int largeCount) {
+		this.airport = a;
+		this.regionalCount = regionalCount;
+		this.smallCount = smallCount;
+		this.mediumCount = medCount;
+		this.largeCount = largeCount;
+	}
+
+	@Override
+	public void process() {
+		List<Aircraft> aircrafts = new ArrayList<Aircraft>();
+		for(int i = 0; i < regionalCount; i++) {
+			aircrafts.add(new RegionalAircraft());
+		}
+		for(int i = 0; i < smallCount; i++) {
+			aircrafts.add(new SmallAircraft());
+		}
+		for(int i = 0; i < mediumCount; i++) {
+			aircrafts.add(new MediumAircraft());
+		}
+		for(int i = 0; i < largeCount; i++) {
+			aircrafts.add(new LargeAircraft());
+		}
+		airport.addAircraftOnGround(aircrafts);
+
+	}
+
+}
