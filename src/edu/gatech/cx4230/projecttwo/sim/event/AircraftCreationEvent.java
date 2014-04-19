67d2f865114b08a29d25db1dfca4ee4c9be84bdf
mcdc09 <ThomasBowling@Comcast.net>
2014-04-19 18:51:48 -0400

    Finished process() for AircraftCreationEvent

--- src/edu/gatech/cx4230/projecttwo/sim/event/AircraftCreationEvent.java
+++ src/edu/gatech/cx4230/projecttwo/sim/event/AircraftCreationEvent.java
@@ -3,12 +3,15 @@ package edu.gatech.cx4230.projecttwo.sim.event;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.gatech.cx4230.projecttwo.sim.main.AirportSimulation;
 import edu.gatech.cx4230.projecttwo.sim.objects.Aircraft;
 import edu.gatech.cx4230.projecttwo.sim.objects.Airport;
+import edu.gatech.cx4230.projecttwo.sim.objects.Flight;
 import edu.gatech.cx4230.projecttwo.sim.objects.LargeAircraft;
 import edu.gatech.cx4230.projecttwo.sim.objects.MediumAircraft;
 import edu.gatech.cx4230.projecttwo.sim.objects.RegionalAircraft;
 import edu.gatech.cx4230.projecttwo.sim.objects.SmallAircraft;
+import edu.gatech.cx4230.projecttwo.sim.objects.World;
 
 /**
  * Object used to pass along initial distribution of aircraft to each airport
@@ -53,7 +56,18 @@ public class AircraftCreationEvent extends AirportEvent {
 		for(int i = 0; i < largeCount; i++) {
 			aircrafts.add(new LargeAircraft());
 		}
-		airport.addAircraftOnGround(aircrafts);
+		
+		for(Aircraft a: aircrafts) {
+			// add aircraft to onTheGround at destination airport
+			airport.addAircraftOnTheGround(a);
+			
+			// consult timetable to get new flight for this aircraft
+			Flight newFlight = World.getTimetable().scheduleFlight(airport, a);
+			// schedule DepartureEvent with destination airport as new origin airport
+			DepartureEvent departEvent = new DepartureEvent(newFlight);
+			airport.addPendingEvent(departEvent);
+			AirportSimulation.addActiveFlight(newFlight);
+		}
 
 	}
 
