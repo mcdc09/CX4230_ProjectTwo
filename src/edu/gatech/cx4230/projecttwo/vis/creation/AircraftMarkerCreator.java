47172d3ca2a8e0f1f1a2bead62b05e513e84881e
mcdc09 <ThomasBowling@Comcast.net>
2014-04-20 22:16:54 -0400

    Redid AircraftMarkers

--- src/edu/gatech/cx4230/projecttwo/vis/creation/AircraftMarkerCreator.java
+++ src/edu/gatech/cx4230/projecttwo/vis/creation/AircraftMarkerCreator.java
@@ -1,71 +1,40 @@
 package edu.gatech.cx4230.projecttwo.vis.creation;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import processing.core.PApplet;
-import de.fhpotsdam.unfolding.geo.Location;
 import de.fhpotsdam.unfolding.marker.Marker;
-import edu.gatech.cx4230.projecttwo.sim.main.SimulationThread;
-import edu.gatech.cx4230.projecttwo.sim.objects.Aircraft;
 import edu.gatech.cx4230.projecttwo.sim.objects.Flight;
-import edu.gatech.cx4230.projecttwo.utilities.GeoHelper;
 import edu.gatech.cx4230.projecttwo.vis.markers.AircraftMarker;
 
 public class AircraftMarkerCreator {
-	private List<Marker> airplaneMarkers;
+	private Map<Integer, AircraftMarker> amMap;
 	
 	public AircraftMarkerCreator(List<Flight> flights, PApplet p, int timeStep) {
-		airplaneMarkers = new ArrayList<Marker>();
+		amMap = new HashMap<Integer, AircraftMarker>();
 		
 		for(Flight f: flights) {
-			Location l = determineAirplaneLocation(f, timeStep);
-			int type = determineAirplaneType(f.getAircraft());
-			int heading = (int) determineAirplaneHeading(f);
-			AircraftMarker am = new AircraftMarker(f.getFlightNumber(), l, type, heading, p);
-			airplaneMarkers.add(am);
+			AircraftMarker am = new AircraftMarker(f, timeStep, p);
+			amMap.put(f.getFlightNumber(), am);
 		} // close for
 	} // close constructor(...)
-	
-	public Location determineAirplaneLocation(Flight f, int timeStep) {
-		double heading = determineAirplaneHeading(f);
-		Location originLoc = f.getOrigin().getLocation();
-		double speed = f.getAircraft().getSpeed(); // km/hr
-		double dtStep = timeStep - f.getTimeOfDeparture(); // steps
-		double dt = dtStep * SimulationThread.timeStep; // sec
-		dt = dt / 3600; // hr
-		double dist = speed * dt; // km
-		
-		Location out = GeoHelper.calcDest(originLoc, heading, dist);
-		return out;
-	}
-	
-	public double determineAirplaneHeading(Flight f) {
-		Location originLoc = f.getOrigin().getLocation();
-		Location destLoc = f.getDestination().getLocation();
-		return GeoHelper.calcHeading(originLoc, destLoc);
-	}
-	
-	public int determineAirplaneType(Aircraft a) {
-		String typeS = a.getAircraftType();
-		int out = -1;
-		if(Aircraft.TYPE_REGIONAL.equals(typeS)) {
-			out = AircraftMarker.REGIONAL;
-		} else if(Aircraft.TYPE_SMALL.equals(typeS)) {
-			out = AircraftMarker.SHORT_RANGE;
-		} else if(Aircraft.TYPE_MEDIUM.equals(typeS)) {
-			out = AircraftMarker.MEDIUM_RANGE;
-		} else if(Aircraft.TYPE_LARGE.equals(typeS)) {
-			out = AircraftMarker.CROSS_COUNTRY;
-		}
-		return out;
-	}
 
 	/**
 	 * @return the airplaneMarkers
 	 */
-	public List<Marker> getAirplaneMarkers() {
-		return airplaneMarkers;
+	public List<AircraftMarker> getAirplaneMarkers() {
+		return new ArrayList<AircraftMarker>(amMap.values());
+	}
+	
+	public List<Marker> getMarkers() {
+		return new ArrayList<Marker>(amMap.values());
+	}
+	
+	public Map<Integer, AircraftMarker> getAircraftMarkerMap() {
+		return amMap;
 	}
 
 }
--- src/edu/gatech/cx4230/projecttwo/vis/markers/AircraftMarker.java
+++ src/edu/gatech/cx4230/projecttwo/vis/markers/AircraftMarker.java
@@ -3,12 +3,15 @@ package edu.gatech.cx4230.projecttwo.vis.markers;
 import java.util.HashMap;
 
 import processing.core.PApplet;
-import processing.core.PConstants;
 import processing.core.PGraphics;
 import processing.core.PImage;
 import de.fhpotsdam.unfolding.geo.Location;
 import de.fhpotsdam.unfolding.marker.AbstractMarker;
+import edu.gatech.cx4230.projecttwo.sim.main.SimulationThread;
+import edu.gatech.cx4230.projecttwo.sim.objects.Aircraft;
+import edu.gatech.cx4230.projecttwo.sim.objects.Flight;
 import edu.gatech.cx4230.projecttwo.utilities.FileHelper;
+import edu.gatech.cx4230.projecttwo.utilities.GeoHelper;
 
 public class AircraftMarker extends AbstractMarker {
 	public static final int REGIONAL = 76486;
@@ -21,22 +24,65 @@ public class AircraftMarker extends AbstractMarker {
 	private static final String PLANE_PURPLE = "drawable/air_plane_purple.png";
 	private static PImage imgReg, imgShort, imgMed, imgCross;
 	private int type;
+	private int flightNumber;
 	private int heading; // degrees from North
 	private static final double SCALING = 0.1;
 	private static int width, height;
+	private Flight flight;
 	
-	public AircraftMarker(int fn, Location location, int type, int heading, PApplet p) {
-		super(location);
+	public AircraftMarker(Flight f, int time, PApplet p) {
+		this.flight = f;
+		this.flightNumber = f.getFlightNumber();
+		this.type = determineAirplaneType(f.getAircraft());
+		this.heading = (int) determineAirplaneHeading();
+		setLocation(calculateLocation(time));
 		
-		this.type = type;
-
 		if(imgReg == null) {
 			loadImages(p);
 		}
 		
 		HashMap<String,Object> prop = new HashMap<String, Object>();
-		prop.put("flightNumber", fn);
+		prop.put("flightNumber", flight.getFlightNumber());
+		prop.put("flight", f);
 		setProperties(prop);
+	}
+	
+	private Location calculateLocation(int time) {
+		Location originLoc = flight.getOrigin().getLocation();
+		
+		double speed = flight.getAircraft().getSpeed(); // km/hr
+		double dtStep = time - flight.getTimeOfDeparture(); // steps
+		double dt = dtStep * SimulationThread.timeStep; // sec
+		dt = dt / 3600; // hr
+		double dist = speed * dt; // km
+		
+		Location out = GeoHelper.calcDest(originLoc, heading, dist);
+		return out;
+	}
+	
+	public void updateLocation(int currTime) {
+		setLocation(calculateLocation(currTime));
+	}
+	
+	public double determineAirplaneHeading() {
+		Location originLoc = flight.getOrigin().getLocation();
+		Location destLoc = flight.getDestination().getLocation();
+		return GeoHelper.calcHeading(originLoc, destLoc);
+	}
+	
+	public int determineAirplaneType(Aircraft a) {
+		String typeS = a.getAircraftType();
+		int out = -1;
+		if(Aircraft.TYPE_REGIONAL.equals(typeS)) {
+			out = AircraftMarker.REGIONAL;
+		} else if(Aircraft.TYPE_SMALL.equals(typeS)) {
+			out = AircraftMarker.SHORT_RANGE;
+		} else if(Aircraft.TYPE_MEDIUM.equals(typeS)) {
+			out = AircraftMarker.MEDIUM_RANGE;
+		} else if(Aircraft.TYPE_LARGE.equals(typeS)) {
+			out = AircraftMarker.CROSS_COUNTRY;
+		}
+		return out;
 	}
 	
 	private void loadImages(PApplet p) {
@@ -59,16 +105,12 @@ public class AircraftMarker extends AbstractMarker {
 		pg.pushStyle();
 		//pg.imageMode(PConstants.CORNER);
 		// The image is drawn in object coordinates, i.e. the marker's origin (0,0) is at its geo-location.
-		pg.rotate((heading + 90)*(PConstants.TWO_PI/360));
-		//pg.translate(x - width/2, y - height / 2);
-		pg.ellipse(x-width/2, y-height/2, width, height);
-		//pg.image(getImageForType(type), x - width/2, y - height / 2, width, height);
-		if(selected) {
-			pg.color(200, 200, 0, 255);
-			pg.ellipse(x - width/2, y - height / 2, width, height);
-		}
-		pg.popStyle();
 		
+		pg.translate(x, y);
+		pg.rotate((float) GeoHelper.convertDegreeToRad(heading));
+		pg.imageMode(PGraphics.CENTER);
+		pg.image(getImageForType(type), 0, 0, width, height);
+		pg.popStyle();
 	}
 
 	@Override
@@ -123,4 +165,10 @@ public class AircraftMarker extends AbstractMarker {
 		this.heading = heading;
 	}
 
+	/**
+	 * @return the flightNumber
+	 */
+	public int getFlightNumber() {
+		return flightNumber;
+	}
 }
