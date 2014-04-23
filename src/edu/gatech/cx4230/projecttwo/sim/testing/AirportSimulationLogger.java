8a1f41d5f92c50444493f16381f21bd518a45036
mcdc09 <ThomasBowling@Comcast.net>
2014-04-23 01:13:53 -0400

    Implemented a Custom Logger for our project
    To print, call the appropriate static method from LoggerMaster

---
+++ src/edu/gatech/cx4230/projecttwo/sim/testing/AirportSimulationLogger.java
@@ -0,0 +1,104 @@
+package edu.gatech.cx4230.projecttwo.sim.testing;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.gatech.cx4230.projecttwo.utilities.FileHelper;
+
+/**
+ * A specific logger for a specific type of event
+ * @author tbowling3
+ *
+ */
+public class AirportSimulationLogger {
+	private List<String> lines;
+	private boolean print;
+	private String partialLine;
+	private String name;
+
+	public AirportSimulationLogger(boolean print, String name) {
+		this.print = print;
+		this.name = name;
+		lines = new ArrayList<String>();
+
+	}
+
+	/**
+	 * Clears the log of lines
+	 */
+	public void clear() {
+		lines.clear();
+	}
+
+	/**
+	 * Saves the log to the specified file
+	 * @param filename
+	 * @param clear
+	 */
+	public void save(String filename, boolean clear) {
+		String out = FileHelper.getPathToResource("Output/" + filename);
+
+		if(!out.endsWith(".txt")) {
+			out = out + ".txt";
+		}
+		try {
+			FileWriter fw = new FileWriter(out);
+			fw.write(this.toString());
+			
+			for(String line: lines) {
+				fw.write(line);
+			} // close for i
+
+			fw.close();
+		} catch(IOException e) {
+			System.err.println("There was a problem writing Log to txt file");
+			e.printStackTrace();
+		}
+		System.out.println("Log saved");
+		if(clear) clear();
+	}
+
+	public void save(boolean clear) {
+		this.save(name, clear);
+	}
+
+	public String toString() {
+		return "ASLogger: " + name + " Line Count: " + lines.size() + "\n";
+	}
+	
+	
+	// Log methods
+		public void logLine(String line) {
+			if(line != null) {
+				if(print) System.out.println(line);
+				if(partialLine != null && !partialLine.isEmpty()) {
+					line += partialLine;
+					partialLine = null;
+				}
+				lines.add(line + "\n");
+
+			}
+		}
+
+		public void log(String partial) {
+			if(partial != null) {
+				partialLine += partial;
+				System.out.print(partial);
+			}
+		}
+
+		public void logLine(Object o) {
+			if(o != null) {
+				logLine(o.toString());
+			}
+		}
+
+		public void log(Object o) {
+			if(o != null) {
+				log(o.toString());
+			}
+		}
+
+}
---
+++ src/edu/gatech/cx4230/projecttwo/sim/testing/AirportSimulationLoggerMaster.java
@@ -0,0 +1,66 @@
+package edu.gatech.cx4230.projecttwo.sim.testing;
+
+/**
+ * Keeps track of the different logger outputs from the simulation
+ * @author tbowling3
+ *
+ */
+public class AirportSimulationLoggerMaster {
+	private static AirportSimulationLogger flightEventLogger = new AirportSimulationLogger(false, "Flight_Events");
+	private static AirportSimulationLogger simulationLogger = new AirportSimulationLogger(false, "Simulation_Logger");
+	
+	// Event Logger
+	public static void logLineEvent(String in) {
+		flightEventLogger.logLine(in);
+	}
+	
+	public static void logEvent(String in) {
+		flightEventLogger.log(in);
+	}
+	
+	public static void logLineEvent(Object in) {
+		flightEventLogger.logLine(in);
+	}
+	
+	public static void logEvent(Object in) {
+		flightEventLogger.log(in);
+	}
+	
+	// Simulation Logger
+	public static void logLineSim(String in) {
+		simulationLogger.logLine(in);
+	}
+	
+	public static void logSim(String in) {
+		simulationLogger.log(in);
+	}
+	
+	public static void logLineSim(Object in) {
+		simulationLogger.logLine(in);
+	}
+	
+	public static void logSim(Object in) {
+		simulationLogger.log(in);
+	}
+	
+	// Overall Methods
+	public static void clear() {
+		simulationLogger.clear();
+		flightEventLogger.clear();
+	}
+	
+	public static void save(String filename, boolean clear) {
+		simulationLogger.save("Sim_" + filename, clear);
+		flightEventLogger.save("Events_" + filename, clear);
+	}
+	
+	public static void save(boolean clear) {
+		simulationLogger.save(clear);
+		flightEventLogger.save(clear);
+	}
+	
+	public static void save() {
+		save(true);
+	}
+
+}
