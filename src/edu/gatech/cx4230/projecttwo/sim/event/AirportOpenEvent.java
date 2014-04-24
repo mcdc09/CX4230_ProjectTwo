102ed8434a3fca0ba3527deaa1374507769bd9fb
mcdc09 <ThomasBowling@Comcast.net>
2014-04-23 22:09:02 -0400

    Created an AirportOpenEvent
    Paired with AirportShutDownEvent

---
+++ src/edu/gatech/cx4230/projecttwo/sim/event/AirportOpenEvent.java
@@ -0,0 +1,36 @@
+package edu.gatech.cx4230.projecttwo.sim.event;
+
+import edu.gatech.cx4230.projecttwo.sim.objects.Airport;
+import edu.gatech.cx4230.projecttwo.sim.objects.Runway;
+
+
+/**
+ * Object paired with an AirportShutDownEvent to simulate a ground stop at a particular
+ * airport.  This will open the runways and all the airport to operate.
+ * @author tbowling3
+ *
+ */
+public class AirportOpenEvent extends AirportEvent {
+	
+	/**
+	 * Creates object
+	 * @param a Airport for the event
+	 * @param timeProcess Time step at which the ground stop is lifted
+	 * @param timeNow The creation time step
+	 */
+	public AirportOpenEvent(Airport a, int timeProcess, int timeNow) {
+		this.airport = a;
+		this.processTime = timeProcess;
+		this.creationTime = timeNow;
+	}
+
+	@Override
+	public void process(int currTime) {
+		airport.setGroundStop(false);
+		for(Runway r: airport.getRunways()) {
+			r.setTimeNextAvailable(processTime);
+		}
+		
+	}
+
+}
