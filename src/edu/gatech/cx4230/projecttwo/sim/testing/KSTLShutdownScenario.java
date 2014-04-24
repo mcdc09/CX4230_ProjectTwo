efa458ab9e574c60a19a8645aa91a1680af8279b
Joseph Mattingly <jmattingly13@gmail.com>
2014-04-23 20:29:20 -0400

    shutdown scenarios
    ATL and STL

---
+++ src/edu/gatech/cx4230/projecttwo/sim/testing/KSTLShutdownScenario.java
@@ -0,0 +1,38 @@
+package edu.gatech.cx4230.projecttwo.sim.testing;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import edu.gatech.cx4230.projecttwo.sim.event.AirportEvent;
+import edu.gatech.cx4230.projecttwo.sim.event.AirportShutDownEvent;
+import edu.gatech.cx4230.projecttwo.sim.main.AirportSimulation;
+import edu.gatech.cx4230.projecttwo.sim.main.SimulationThread;
+import edu.gatech.cx4230.projecttwo.sim.objects.World;
+
+/**
+ * Simulation scenario object that shuts down a large airport like Atlanta
+ * @author Joseph Mattingly
+ *
+ */
+public class KSTLShutdownScenario extends SimulationScenario {
+	/**
+	 * The total amount of time for the simulation to run in units of seconds
+	 */
+	public static final int TIME_RUN = 24*60*60;
+	private static List<AirportEvent> failures = new ArrayList<AirportEvent>();
+	private static boolean f = failures.add(new AirportShutDownEvent(World.getAirport("STL"), 500));
+	
+	public KSTLShutdownScenario(boolean vis, List<AirportEvent> failurez, int totalAircraft) {
+		super(vis, failures, 100);
+	}
+
+	@Override
+	public boolean continueSimulation(AirportSimulation sim) {
+		int timeS = sim.getTimeStep() * SimulationThread.TIME_PER_STEP; // seconds
+		return timeS <= TIME_RUN;
+	}
+
+	@Override
+	public void quitSimulation(AirportSimulation sim) {}
+}
