4a65556eb0d699cb0321388f5296a5897529250f
mcdc09 <ThomasBowling@Comcast.net>
2014-04-24 21:36:19 -0400

    Moved CSV write functions to a new Class

--- src/edu/gatech/cx4230/projecttwo/utilities/ArrayManipulation.java
+++ src/edu/gatech/cx4230/projecttwo/utilities/ArrayManipulation.java
@@ -180,68 +180,6 @@ public class ArrayManipulation {
 	} // close function
 
 	/**
-	 * Writes an array to a comma-separated-variable (CSV) file to the directory of this application
-	 * @param array Array to be saved to the file
-	 * @param filename Name of the csv File
-	 */
-	public static void writeDoubleArrayToCSV(double[][] array, String filename) {
-		try {
-			if(!filename.endsWith(".csv")) {
-				filename = filename + ".csv";
-			}
-			if(array != null) {
-				FileWriter fw = new FileWriter(filename);
-				for(int i = 0; i < array.length; i++) {
-					String line = "";
-
-					for(int j = 0; j < array[i].length; j++) {
-						if(j != 0) line += ", ";
-						line += array[i][j];
-					} // close for j
-					line += "\n";
-					fw.write(line);
-				} // close for i
-
-				fw.close();
-			} // close if null
-		} catch (IOException e) {
-			System.err.println("There was a problem writing Array to CSV file");
-			e.printStackTrace();
-		} // close catch
-	} // close method
-
-	/**
-	 * Writes an array to a comma-separated-variable (CSV) file to the directory of this application
-	 * @param array Array to be saved to the file
-	 * @param filename Name of the csv File
-	 */
-	public static void writeIntArrayToCSV(int[][] array, String filename) {
-		try {
-			if(!filename.endsWith(".csv")) {
-				filename = filename + ".csv";
-			}
-			if(array != null) {
-				FileWriter fw = new FileWriter(filename);
-				for(int i = 0; i < array.length; i++) {
-					String line = "";
-
-					for(int j = 0; j < array[i].length; j++) {
-						if(j != 0) line += ", ";
-						line += array[i][j];
-					} // close for j
-					line += "\n";
-					fw.write(line);
-				} // close for i
-
-				fw.close();
-			} // close if null
-		} catch (IOException e) {
-			System.err.println("There was a problem writing Array to CSV file");
-			e.printStackTrace();
-		} // close catch
-	} // close method
-
-	/**
 	 * Converts a 2D array of booleans to a 2D array of doubles where
 	 * 1 is true and 0 is false
 	 * @param in 2D boolean array to be converted to an array of doubles
